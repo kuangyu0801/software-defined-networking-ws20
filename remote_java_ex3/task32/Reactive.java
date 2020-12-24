0575b3d446225b897b038ac618c090dcb7e518a6
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-12-24 21:32:43 +0100

    feat: successfully get dpids and links from server

--- remote_java_ex3/task32/Reactive.java
+++ remote_java_ex3/task32/Reactive.java
@@ -12,7 +12,6 @@ import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.types.DatapathId;
 
-
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
@@ -31,8 +30,8 @@ public class Reactive implements IFloodlightModule, IOFMessageListener {
 	protected IFloodlightProviderService floodlightProvider;
 	protected IOFSwitchService switchService;
 	protected ILinkDiscoveryService linkDiscoverer;
-	private Set<DatapathId> setDatapathId;
-
+	private Set<DatapathId> setDpids;
+	private Map<DatapathId, Set<Link>> mapSwitchLinks;
 	// TODO: export logger output into log file
 	private static final Logger logger = Logger.getLogger(Reactive.class.getSimpleName());
 	// static 
@@ -105,25 +104,72 @@ public class Reactive implements IFloodlightModule, IOFMessageListener {
 		// DONE Auto-generated method stub
 		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
 		logger.info("Start Up");
-		logger.info("Get all DPID");
-		setDatapathId = switchService.getAllSwitchDpids();
-		// wait until all the switches has been add to network
-		// TODO: remove this method to a more steady state
-		while (setDatapathId.size() < 10) {
-			setDatapathId = switchService.getAllSwitchDpids();
-		}
-		for (DatapathId dpid: setDatapathId) {
-			logger.info(dpid.toString());
-		}
-		logger.info("Total switch number:" + setDatapathId.size());
-		Map<Link, LinkInfo> links = linkDiscoverer.getLinks();
-		logger.info("Get all links");
-		// TODO: remove this method to a more steady state
-		for (Link link : links.keySet()) {
-			logger.info(link.toString());
-		}
-
+		enableReactiveRouting();
 		logger.info("Start Up End");
 	}
 
+	private void enableReactiveRouting() {
+		// STEP-1: link state detection: create directed graph for djikstra
+		createNetworkGraph();
+		// TODO: STEP-2: calculate shortest path: using dijkstra
+
+		// TODO: STEP-3: create flow
+
+		// TODO: STEP-4: install flow (when PACKET_IN ? or install right away
+
+		// TODO: verification with ping and routetrace
+	}
+
+	// TODO: STEP-1: link state detection: create directed graph for djikstra
+	private void createNetworkGraph() {
+		logger.info("Get all DatapathID");
+		setDpids = switchService.getAllSwitchDpids();
+
+		// TODO: find out a way to avoid this part
+		// wait until all the switches has been add to network
+		while (setDpids.size() < 10) {
+			setDpids = switchService.getAllSwitchDpids();
+		}
+
+		logger.info("Total switch number: " + setDpids.size());
+		logger.info("Printing all DatapathID");
+
+		for (DatapathId dpid: setDpids) {
+			logger.info(dpid.toString());
+		}
+
+		logger.info("Getting switch-link map");
+
+		mapSwitchLinks = linkDiscoverer.getSwitchLinks();
+		while (mapSwitchLinks.size() < 10) {
+			mapSwitchLinks = linkDiscoverer.getSwitchLinks();
+			logger.info("Current switch links map size: " + mapSwitchLinks.size());
+		}
+
+		logger.info("Get all links");
+
+		// TODO: remove this method to a more steady state
+		for (DatapathId dpid : setDpids) {
+			logger.info("Links of switch" + dpid.toString());
+			mapSwitchLinks.containsKey(dpid);
+			for (Link link : mapSwitchLinks.get(dpid)) {
+				logger.info(link.toString());
+			}
+		}
+	}
+
+	// TODO: STEP-2: calculate shortest path: using dijkstra
+	private void calShortestPath() {
+
+	}
+
+	// TODO: STEP-3: ceate flow
+	private void createFlow() {
+
+	}
+
+	// TODO: STEP-4: install flow (when PACKET_IN ? or install right away
+	private void installFlow() {
+
+	}
 }
