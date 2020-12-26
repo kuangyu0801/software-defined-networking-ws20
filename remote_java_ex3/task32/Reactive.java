7d8841443f8db6588df12ab9488cd913cc7bff7d
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-12-26 20:21:54 +0100

    feat: porting dijkstra & initMap from src code and run ok

--- remote_java_ex3/task32/Reactive.java
+++ remote_java_ex3/task32/Reactive.java
@@ -3,7 +3,9 @@ package net.sdnlab.ex3.task32;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -21,19 +23,29 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.types.NodePortTuple;
 import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
 import net.floodlightcontroller.linkdiscovery.Link;
 import net.floodlightcontroller.linkdiscovery.internal.LinkInfo;
+import net.floodlightcontroller.routing.BroadcastTree;
+import net.floodlightcontroller.topology.TopologyInstance;
 
 public class Reactive implements IFloodlightModule, IOFMessageListener {
+	
+	private static final int MAX_LINK_WEIGHT = 10000;
+	private static final int MAX_PATH_WEIGHT = Integer.MAX_VALUE - MAX_LINK_WEIGHT - 1;
+	private static final int HOPCOUNT = 1;
+	private static final int UTILIZATION = 2;
+	private static final Logger logger = Logger.getLogger(Reactive.class.getSimpleName());
+
 	// Since we are listening to OpenFlow messages we need to register with the FloodlightProvider (IFloodlightProviderService class)
 	protected IFloodlightProviderService floodlightProvider;
 	protected IOFSwitchService switchService;
 	protected ILinkDiscoveryService linkDiscoverer;
+
 	private Set<DatapathId> setDpids;
 	private Map<DatapathId, Set<Link>> mapSwitchLinks;
-	// TODO: export logger output into log file
-	private static final Logger logger = Logger.getLogger(Reactive.class.getSimpleName());
+
 	// static 
 	@Override
 	public String getName() {
@@ -81,6 +93,7 @@ public class Reactive implements IFloodlightModule, IOFMessageListener {
 	}
 	
 	private void setupLogger() {
+		// DONE: export logger output into log file
 		try {
 			FileHandler fileHandler = new FileHandler("/home/student/ex3/task32.log");
 			logger.addHandler(fileHandler);
@@ -120,7 +133,7 @@ public class Reactive implements IFloodlightModule, IOFMessageListener {
 		// TODO: verification with ping and routetrace
 	}
 
-	// TODO: STEP-1: link state detection: create directed graph for djikstra
+	// DONE: STEP-1: link state detection: create directed graph for djikstra
 	private void createNetworkGraph() {
 		logger.info("Get all DatapathID");
 		setDpids = switchService.getAllSwitchDpids();
@@ -171,5 +184,262 @@ public class Reactive implements IFloodlightModule, IOFMessageListener {
 	// TODO: STEP-4: install flow (when PACKET_IN ? or install right away
 	private void installFlow() {
 
+	}
+
+	/*
+	 * Creates a map of links and the cost associated with each link
+	 *  Reference implementation: https://github.com/floodlight/floodlight/blob/d737cb05656a6038f4e2277ffb4503d45b7b29cb/src/main/java/net/floodlightcontroller/topology/TopologyInstance.java#L657
+	 * */
+	public Map<Link,Integer> initLinkCostMap(int type, Map<DatapathId, Set<Link>> links) {
+		Map<Link, Integer> linkCost = new HashMap<Link, Integer>();
+		//int tunnel_weight = portsWithLinks.size() + 1;
+
+		switch (type){
+/*			case HOPCOUNT_AVOID_TUNNELS:
+				log.debug("Using hop count with tunnel bias for metrics");
+				for (NodePortTuple npt : portsTunnel) {
+					if (links.get(npt) == null) {
+						continue;
+					}
+					for (Link link : links.get(npt)) {
+						if (link == null) {
+							continue;
+						}
+						linkCost.put(link, tunnel_weight);
+					}
+				}
+				return linkCost;*/
+
+			case HOPCOUNT:
+				logger.info("Using hop count w/o tunnel bias for metrics");
+				for (DatapathId npt : links.keySet()) {
+					if (links.get(npt) == null) {
+						continue;
+					}
+					for (Link link : links.get(npt)) {
+						if (link == null) {
+							continue;
+						}
+						linkCost.put(link,1);
+					}
+				}
+				return linkCost;
+
+/*			case LATENCY:
+				log.debug("Using latency for path metrics");
+				for (NodePortTuple npt : links.keySet()) {
+					if (links.get(npt) == null) {
+						continue;
+					}
+					for (Link link : links.get(npt)) {
+						if (link == null) {
+							continue;
+						}
+						if ((int)link.getLatency().getValue() < 0 ||
+								(int)link.getLatency().getValue() > MAX_LINK_WEIGHT) {
+							linkCost.put(link, MAX_LINK_WEIGHT);
+						} else {
+							linkCost.put(link,(int)link.getLatency().getValue());
+						}
+					}
+				}
+				return linkCost;*/
+
+/*			case LINK_SPEED:
+				TopologyManager.statisticsService.collectStatistics(true);
+				log.debug("Using link speed for path metrics");
+				for (NodePortTuple npt : links.keySet()) {
+					if (links.get(npt) == null) {
+						continue;
+					}
+					long rawLinkSpeed = 0;
+					IOFSwitch s = TopologyManager.switchService.getSwitch(npt.getNodeId());
+					if (s != null) {
+						OFPortDesc p = s.getPort(npt.getPortId());
+						if (p != null) {
+							rawLinkSpeed = p.getCurrSpeed();
+						}
+					}
+					for (Link link : links.get(npt)) {
+						if (link == null) {
+							continue;
+						}
+
+						if ((rawLinkSpeed / 10^6) / 8 > 1) {
+							int linkSpeedMBps = (int)(rawLinkSpeed / 10^6) / 8;
+							linkCost.put(link, (1/linkSpeedMBps)*1000);
+						} else {
+							linkCost.put(link, MAX_LINK_WEIGHT);
+						}
+					}
+				}
+				return linkCost;*/
+
+/*			case UTILIZATION:
+				TopologyManager.statisticsService.collectStatistics(true);
+				log.debug("Using utilization for path metrics");
+				for (NodePortTuple npt : links.keySet()) {
+					if (links.get(npt) == null) continue;
+					SwitchPortBandwidth spb = TopologyManager.statisticsService
+							.getBandwidthConsumption(npt.getNodeId(), npt.getPortId());
+					long bpsTx = 0;
+					if (spb != null) {
+						bpsTx = spb.getBitsPerSecondTx().getValue();
+					}
+					for (Link link : links.get(npt)) {
+						if (link == null) {
+							continue;
+						}
+
+						if ((bpsTx / 10^6) / 8 > 1) {
+							int cost = (int) (bpsTx / 10^6) / 8;
+							linkCost.put(link, cost);
+						} else {
+							linkCost.put(link, MAX_LINK_WEIGHT);
+						}
+					}
+				}
+				return linkCost;*/
+
+			default:
+				logger.info("Invalid Selection: Using Default Hop Count with Tunnel Bias for Metrics");
+/*				for (NodePortTuple npt : portsTunnel) {
+					if (links.get(npt) == null) continue;
+					for (Link link : links.get(npt)) {
+						if (link == null) continue;
+						linkCost.put(link, tunnel_weight);
+					}
+				}*/
+				return linkCost;
+		}
+	}
+
+	// reference: https://github.com/floodlight/floodlight/blob/d737cb05656a6038f4e2277ffb4503d45b7b29cb/src/main/java/net/floodlightcontroller/topology/TopologyInstance.java#L444
+	private class NodeDist implements Comparable<NodeDist> {
+		private final DatapathId node;
+		public DatapathId getNode() {
+			return node;
+		}
+
+		private final int dist;
+		public int getDist() {
+			return dist;
+		}
+
+		public NodeDist(DatapathId node, int dist) {
+			this.node = node;
+			this.dist = dist;
+		}
+
+		@Override
+		public int compareTo(NodeDist o) {
+			if (o.dist == this.dist) {
+				return (int)(this.node.getLong() - o.node.getLong());
+			}
+			return this.dist - o.dist;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			NodeDist other = (NodeDist) obj;
+			if (node == null) {
+				if (other.node != null)
+					return false;
+			} else if (!node.equals(other.node))
+				return false;
+			return true;
+		}
+
+		@Override
+		public int hashCode() {
+			assert false : "hashCode not designed";
+			return 42;
+		}
+	}
+
+	/*
+	 * Dijkstra that calculates destination rooted trees over the entire topology.
+	 * reference implementation: https://github.com/floodlight/floodlight/blob/d737cb05656a6038f4e2277ffb4503d45b7b29cb/src/main/java/net/floodlightcontroller/topology/TopologyInstance.java#L578
+	 */
+	private BroadcastTree dijkstra(Map<DatapathId, Set<Link>> links, DatapathId root,
+								   Map<Link, Integer> linkCost,
+								   boolean isDstRooted) {
+		HashMap<DatapathId, Link> nexthoplinks = new HashMap<DatapathId, Link>();
+		HashMap<DatapathId, Integer> cost = new HashMap<DatapathId, Integer>();
+		int w;
+
+		for (DatapathId node : links.keySet()) {
+			nexthoplinks.put(node, null);
+			cost.put(node, MAX_PATH_WEIGHT);
+			//log.debug("Added max cost to {}", node);
+		}
+
+		HashMap<DatapathId, Boolean> seen = new HashMap<DatapathId, Boolean>();
+		PriorityQueue<NodeDist> nodeq = new PriorityQueue<NodeDist>();
+		nodeq.add(new NodeDist(root, 0));
+		cost.put(root, 0);
+
+		//log.debug("{}", links);
+
+		while (nodeq.peek() != null) {
+			NodeDist n = nodeq.poll();
+			DatapathId cnode = n.getNode();
+			int cdist = n.getDist();
+
+			if (cdist >= MAX_PATH_WEIGHT) break;
+			if (seen.containsKey(cnode)) continue;
+			seen.put(cnode, true);
+
+			//log.debug("cnode {} and links {}", cnode, links.get(cnode));
+			if (links.get(cnode) == null) continue;
+			for (Link link : links.get(cnode)) {
+				DatapathId neighbor;
+
+				if (isDstRooted == true) {
+					neighbor = link.getSrc();
+				} else {
+					neighbor = link.getDst();
+				}
+
+				// links directed toward cnode will result in this condition
+				if (neighbor.equals(cnode)) continue;
+
+				if (seen.containsKey(neighbor)) continue;
+
+				if (linkCost == null || linkCost.get(link) == null) {
+					w = 1;
+				} else {
+					w = linkCost.get(link);
+				}
+
+				int ndist = cdist + w; // the weight of the link, always 1 in current version of floodlight.
+				logger.info("Neighbor: " + neighbor);
+				logger.info("Cost: "+ cost);
+				logger.info("Neighbor cost: " + cost.get(neighbor));
+
+				if (ndist < cost.get(neighbor)) {
+					cost.put(neighbor, ndist);
+					nexthoplinks.put(neighbor, link);
+
+					NodeDist ndTemp = new NodeDist(neighbor, ndist);
+					// Remove an object that's already in there.
+					// Note that the comparison is based on only the node id,
+					// and not node id and distance.
+					nodeq.remove(ndTemp);
+					// add the current object to the queue.
+					nodeq.add(ndTemp);
+				}
+			}
+		}
+
+		BroadcastTree ret = new BroadcastTree(nexthoplinks, cost);
+
+		return ret;
 	}
 }
