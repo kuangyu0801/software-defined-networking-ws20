21469e0d809dc33cd58a402368653b2a9a5ab295
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-01-19 20:04:26 +0100

    feat: add task3 skeleton

---
+++ remote_java_ex4/task43/SubscriptionResource.java
@@ -0,0 +1,29 @@
+package net.sdnlab.ex4.task43;
+
+import org.restlet.resource.Delete;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+
+public class SubscriptionResource extends ServerResource {
+
+	@Post
+	public String postSubscription(String json) {
+		ITask43Service task43Service = (ITask43Service) getContext().getAttributes()
+				.get(ITask43Service.class.getCanonicalName());
+		String name = (String) getRequestAttributes().get("name");
+
+		// TODO: parse JSON
+		System.out.println("Post Request: "+json);
+
+		return task43Service.addSubscription(name /* TODO: add arguments */);
+	}
+
+	@Delete
+	public String deleteSubscription() {
+		ITask43Service task43Service = (ITask43Service) getContext().getAttributes()
+				.get(ITask43Service.class.getCanonicalName());
+		String name = (String) getRequestAttributes().get("name");
+		return task43Service.deleteSubscription(name);
+	}
+
+}
