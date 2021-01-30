6fa7d634167253945ef2aacba7203634ef6ed705
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-01-30 20:55:35 +0100

    feat: finish basic monitor

--- remote_sshfs_ex5/simple_monitor.py
+++ remote_sshfs_ex5/simple_monitor.py
@@ -12,5 +12,11 @@ ip1  = IPAddr('123.45.67.89')
 # TODO
 monitor = flood()
 
+def printer(pkt): 
+    print pkt
+    
+queryPolicy = packets() 
+queryPolicy.register_callback(printer)
+    
 def main():
-    return monitor
+    return match(dstip=ip_srv, dstport=9090, ethtype=packet.IPV4, protocol=packet.TCP_PROTO) | match(scrip=ip_h2) >> queryPolicy
